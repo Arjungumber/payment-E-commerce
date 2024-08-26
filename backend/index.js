1ac6da935d140de51d4e445fadcc13f807682264
Arjun Gumber <101098267+Arjungumber@users.noreply.github.com>
2024-08-26 20:23:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,8 @@ app.use(
 );
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
   next();
 });
 
